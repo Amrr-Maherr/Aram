ec395d7c4f8d0aaa8065174a2fef3229d2226d08
Amr maher <amrm80233@>
2025-03-29 15:39:03 +0200

    update

--- src/app/Components/Button.jsx
+++ src/app/Components/Button.jsx
@@ -1,8 +1,8 @@
-export default function Button() {
+export default function Button({text}) {
     return (
       <>
         <button className="w-[200px] cursor-pointer sm:w-[250px] md:w-[294px] h-[40px] sm:h-[48px] md:h-[56px] bg-[#1E3A8A] rounded-[10px] text-base sm:text-lg md:text-xl font-[600]">
-          اكتشف المزيد
+          {text}
         </button>
       </>
     );
