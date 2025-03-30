3c41a94bd854d0370934bed224dd922c4d7d5572
Amr maher <amrm80233@>
2025-03-30 12:29:20 +0200

    update

--- src/app/Components/AboutUs/AboutUsSection.jsx
+++ src/app/Components/AboutUs/AboutUsSection.jsx
@@ -1,33 +1,13 @@
-"use client"
-import { useEffect } from "react";
 import AboutUsImage from "./AboutUsImage";
 import AboutUsText from "./AboutUsText";
-import ScrollReveal from "scrollreveal";
 import Button from "../Button";
 
 export default function AboutUsSection() {
-  useEffect(() => {
-    // إعداد ScrollReveal
-    ScrollReveal().reveal(".about-us-image", {
-      duration: 1000,
-      origin: "left",
-      distance: "100px",
-      opacity: 0,
-      delay: 200,
-    });
 
-    ScrollReveal().reveal(".about-us-text", {
-      duration: 1000,
-      origin: "right",
-      distance: "100px",
-      opacity: 0,
-      delay: 400,
-    });
-  }, []);
 
   return (
     <div className="h-fit">
-      <div className="container w-[90%] mx-auto overflow-x-hidden flex items-center justify-between flex-wrap md:flex-nowrap my-[100px] gap-24">
+      <div className="container w-[90%] mx-auto overflow-x-hidden flex items-center justify-between flex-wrap md:flex-nowrap my-[100px] gap-10">
         <div className="about-us-image">
           <AboutUsImage />
         </div>
--- src/app/Components/Services/ServicesBox.jsx
+++ src/app/Components/Services/ServicesBox.jsx
@@ -1,22 +1,8 @@
-"use client"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { useEffect } from "react";
-import ScrollReveal from "scrollreveal"; // استيراد المكتبة
 
 export default function ServicesBox({ ele }) {
-  useEffect(() => {
-    ScrollReveal().reveal(".service-box", {
-      duration: 1000, 
-      distance: "50px",
-      origin: "bottom",
-      opacity: 0,
-      delay: 200,
-      interval: 200,
-    });
-  }, []);
-
   return (
-    <div className="service-box w-[330px] h-[194px] p-4 flex flex-col items-end justify-between shadow-lg rounded-lg mx-auto my-5">
+    <div className="w-[330px] h-[194px]  p-4 flex flex-col items-end justify-between shadow-lg rounded-lg mx-auto my-5">
       <div className="text-3xl text-[#8A1E70]">
         <FontAwesomeIcon icon={ele.icon} />
       </div>
