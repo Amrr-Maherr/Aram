9122d22e540620ebf90590af916b5f58d1e82000
Amr maher <amrm80233@>
2025-03-30 11:57:49 +0200

    update

--- src/app/Components/AboutUs/AboutUsSection.jsx
+++ src/app/Components/AboutUs/AboutUsSection.jsx
@@ -3,6 +3,7 @@ import { useEffect } from "react";
 import AboutUsImage from "./AboutUsImage";
 import AboutUsText from "./AboutUsText";
 import ScrollReveal from "scrollreveal";
+import Button from "../Button";
 
 export default function AboutUsSection() {
   useEffect(() => {
@@ -26,12 +27,13 @@ export default function AboutUsSection() {
 
   return (
     <div className="h-fit">
-      <div className="container w-[90%] mx-auto overflow-x-hidden flex items-center justify-between flex-wrap md:flex-nowrap my-[100px] gap-10">
+      <div className="container w-[90%] mx-auto overflow-x-hidden flex items-center justify-between flex-wrap md:flex-nowrap my-[100px] gap-24">
         <div className="about-us-image">
           <AboutUsImage />
         </div>
-        <div className="about-us-text">
+        <div className="about-us-text flex items-center flex-col justify-end gap-10">
           <AboutUsText />
+          <Button text="قراءه المزيد" />
         </div>
       </div>
     </div>
--- src/app/Components/Button.jsx
+++ src/app/Components/Button.jsx
@@ -1,7 +1,7 @@
 export default function Button({text}) {
     return (
       <>
-        <button className="w-[200px] cursor-pointer sm:w-[250px] md:w-[294px] h-[40px] sm:h-[48px] md:h-[56px] bg-[#1E3A8A] rounded-[10px] text-base sm:text-lg md:text-xl font-[600]">
+        <button className="w-[200px] cursor-pointer sm:w-[250px] md:w-[294px] h-[40px] sm:h-[48px] md:h-[56px] bg-[#1E3A8A] rounded-[10px] text-base sm:text-lg md:text-xl font-[600] text-white">
           {text}
         </button>
       </>
