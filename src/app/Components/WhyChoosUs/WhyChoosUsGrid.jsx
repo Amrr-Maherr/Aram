6f5adac271d8bac3364a5eddc7dddab241ada14f
Amr maher <amrm80233@>
2025-03-30 12:33:15 +0200

    update

--- src/app/Components/WhyChoosUs/WhyChoosUsGrid.jsx
+++ src/app/Components/WhyChoosUs/WhyChoosUsGrid.jsx
@@ -4,14 +4,14 @@ export default function WhyChoosUsGrid() {
       <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
         <div className="relative h-full">
           <img
-            src="/Assets/7728019141697440863.png"
+            src="/Assets/thumbnail_fndk-aano-n-lhmr_l1gCKqCe.png"
             alt="Main Image"
             className="w-full h-full object-cover rounded-lg"
           />
         </div>
         <div className="flex flex-col gap-4">
           <img
-            src="/Assets/7728019141697440863.png"
+            src="/Assets/images (5).png"
             alt="First Image"
             className="w-full h-[50%] object-cover rounded-lg"
           />
